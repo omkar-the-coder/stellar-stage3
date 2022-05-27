3b9507983763cd1a332ed8ea4748a71db00c3f2b
omkar-the-coder <102938921+omkar-the-coder@users.noreply.github.com>
2022-05-27 18:51:03 +0530

    Add files via upload

--- project_template/screens/Home.js
+++ project_template/screens/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
     View,
     Text,
@@ -8,44 +8,59 @@ import {
     Platform,
     StatusBar,
     ImageBackground,
-    Image,
+    Image
 } from "react-native";
 
 export default class HomeScreen extends Component {
+
+
     render() {
         return (
             <View style={styles.container}>
                 <SafeAreaView style={styles.droidSafeArea} />
-                <ImageBackground
-                source={require('../bg_images.png')}
-                resizeMode="cover"
-                style={styles.backgroundImages}>
+                <ImageBackground source={require('../assets/stars.gif')} style={styles.backgroundImage}>
                     <View style={styles.titleBar}>
+                        <Image source={require("../assets/main-icon.png")} style={{ width: 150, height: 150 }}></Image>
                         <Text style={styles.titleText}>Stellar</Text>
                         <Text style={styles.titleText}>App</Text>
                     </View>
-                    <TouchableOpacity style={styles.routeCard}>
+
+                    <TouchableOpacity style={styles.routeCard} onPress={() =>
+                        this.props.navigation.navigate("SpaceCraft")
+                    }>
                         <Text style={styles.routeText}>Spacecrafts</Text>
+                        <Image source={require("../assets/space_crafts.png")} style={styles.routeImage}></Image>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.routeCard}>
+                    <TouchableOpacity style={styles.routeCard} onPress={() =>
+                        this.props.navigation.navigate("StarMap")
+                    }>
                         <Text style={styles.routeText}>Star Map</Text>
+                        <Image source={require("../assets/star_map.png")} style={styles.routeImage}></Image>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.routeCard}>
+                    <TouchableOpacity style={styles.routeCard} onPress={() =>
+                        this.props.navigation.navigate("DailyPic")
+                    }>
                         <Text style={styles.routeText}>Daily Pictures</Text>
+                        <Image source={require("../assets/daily_pictures.png")} style={styles.routeImage}></Image>
                     </TouchableOpacity>
-                    </ImageBackground>
+                </ImageBackground>
             </View>
-        )
+        );
     }
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1
     },
     droidSafeArea: {
         marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
     },
     routeCard: {
         flex: 0.12,
@@ -74,16 +89,12 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center"
     },
-    backgroundImage:{
-        flex:1,
-        resizeMode:'cover',
-    },
-    routeImage:{
-        position:'absolute',
-        top:'-20',
-        right:'-15',
-        height:'80',
-        resizeMode:'contain',
+    routeImage: {
+        position: "absolute",
+        top: -20,
+        right: -15,
+        height: 80,
+        width: 80,
+        resizeMode: "contain"
     }
-
 });
